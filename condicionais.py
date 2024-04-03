3f87c0c4204116f1894ca28dbc8ff189a9795fe7
victor samuel oliveira <victorsamueloliveira59@gmail.com>
2024-04-02 21:49:04 -0300

    Refactor Exerc 08

--- condicionais.py
+++ condicionais.py
@@ -1,27 +1,36 @@
-def verificar_maioridade():
-    idade = int(input('Qual é a sua idade: '))
-    if idade >= 18:
-        print("Maior de idade")
-    else:
-        print("Menor de idade")
-
-verificar_maioridade()
-
-
-def par_ou_impar():
-    numero = int(input('Digite um número:'))
-    if numero % 2 == 0:
-             print("Este número é par")
-    else:
-             print("Este número é impar")
-
-
-par_ou_impar()
+# def verificar_maioridade():
+#     idade = int(input('Qual é a sua idade: '))
+#     if idade >= 18:
+#         print("Maior de idade")
+#     else:
+#         print("Menor de idade")
+#
+# verificar_maioridade()
+#
+#
+# def par_ou_impar():
+#     numero = int(input('Digite um número:'))
+#     if numero % 2 == 0:
+#              print("Este número é par")
+#     else:
+#              print("Este número é impar")
+#
+#
+# par_ou_impar()
 
 def nota_aprovacao():
-    nota1 = float(input('Digite a nota: '))
-    if nota1 >= 7:
-        print("Aprovado")
-    else: print("Reprovado")
+        try:
+            nota1 = float(input('Digite a nota: '))
+        except ValueError:
+            print("Valor inválido. Digite um número entre 0 e 10.")
+            return
+
+        if 0 <= nota1 <= 10:
+            if nota1 >= 7:
+                print("Approvado")
+            else:
+                print("Reprovado")
+        else:
+            print("Valor não estar entre 0 e 10.")
 
 nota_aprovacao()

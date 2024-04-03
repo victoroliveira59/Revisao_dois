2f500f578ff0941faff074759eb915dcae5069ff
victor samuel oliveira <victorsamueloliveira59@gmail.com>
2024-04-02 21:00:26 -0300

    Resolve questão 07

--- condicionais.py
+++ condicionais.py
@@ -6,3 +6,15 @@ def verificar_maioridade():
         print("Menor de idade")
 
 verificar_maioridade()
+
+
+def par_ou_impar():
+    numero = int(input('Digite um número:'))
+    if numero % 2 == 0:
+             print("Este número é par")
+    else:
+             print("Este número é impar")
+
+
+par_ou_impar()
+
